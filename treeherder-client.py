e1a26eac26d0967e7be8e06497e05625fc46de13
Aaron Train <aaron.train@gmail.com>
2021-07-21 16:45:17 -0400

    Major refactoring and error checking

--- treeherder-client.py
+++ treeherder-client.py
@@ -71,7 +71,12 @@ class THClient:
 def main():
     args = parse_args(sys.argv[1:])
     config.read(args.config)
-    project_config.read(args.project + ".ini")
+
+    if os.path.isfile(args.project + '.ini'):
+        project_config.read(args.project + '.ini')
+    else:
+        print('Project configuration not found')
+        sys.exit(1)
 
     try:
         c = THClient()
@@ -90,207 +95,214 @@ def main():
     outcomes = []
     dataset = []
 
-    print('Fetched Push data from TreeHerder..')
-    print('Fetching recent {0} in {1} ({2} pushes)\n'.format(
-            project_config['job']['result'],
-            project_config['job']['symbol'],
+    for job in project_config.sections():     
+        print('Fetched Push data from TreeHerder..')
+        print('Fetching recent {0} in {1} ({2} pushes)\n'.format(
+            project_config[job]['result'],
+            project_config[job]['symbol'],
             config['pushes']['maxcount']
-        )
-    )
-
-    for _push in sorted(p, key=lambda push: push['id']):
-        jobs = c.client.get_jobs(
-            project=args.project,
-            push_id=_push['id'],
-            tier=project_config['job']['tier'],
-            job_type_symbol=project_config['job']['symbol'],
-            result=project_config['job']['result'],
-            job_group_symbol=project_config['job']['group_symbol'],
-            who=config['filters']['author']
+           )
         )
 
-        for _job in jobs:
-            _matrix_outcome_details = None
-            _test_details = []
-            _revSHA = None
-
-            _log = c.client.get_job_log_url(
+        for _push in sorted(p, key=lambda push: push['id']):
+            jobs = c.client.get_jobs(
                 project=args.project,
-                job_id=_job['id']
+                push_id=_push['id'],
+                tier=project_config[job]['tier'],
+                job_type_symbol=project_config[job]['symbol'],
+                result=project_config[job]['result'],
+                job_group_symbol=project_config[job]['group_symbol'],
+                who=config['filters']['author']
             )
-            for _log_url in _log:
-                _log = _log_url['url']
 
-            # TaskCluster
-            try:
-                # Dependent on public artifact visibility
-                if (re.compile("^(ui-){1}.*")).search(
-                    project_config['job']['symbol']
-                ):
-                    # Matrix
-                    with request.urlopen(
-                        '{0}/{1}/{2}/public/results/{3}'.format(
-                            config['taskcluster']['artifacts'],
-                            _job['task_id'],
-                            _job['retry_id'],
-                            config['artifacts']['matrix']
+            for _job in jobs:
+                _matrix_outcome_details = None
+                _test_details = []
+                _revSHA = None
+
+                _log = c.client.get_job_log_url(
+                    project=args.project,
+                    job_id=_job['id']
+                )
+                for _log_url in _log:
+                    _log = _log_url['url']
+
+                # TaskCluster
+                try:
+                    # Dependent on public artifact visibility
+                    if (re.compile("^(ui-){1}.*")).search(
+                        project_config[job]['symbol']
+                    ):
+                        # Matrix
+                        with request.urlopen(
+                            '{0}/{1}/{2}/public/results/{3}'.format(
+                                config['taskcluster']['artifacts'],
+                                _job['task_id'],
+                                _job['retry_id'],
+                                config['artifacts']['matrix']
+                            )
+                        ) as resp:
+                            source = resp.read()
+                            data = json.loads(source)
+                            for key, value in data.items():
+                                _matrix_general_details = {
+                                    "webLink": value[
+                                        'webLinkWithoutExecutionDetails'
+                                    ]
+                                }
+                                _matrix_outcome_details = value['axes'][0]
+
+                        # JUnitReport
+                        with request.urlopen(
+                            '{0}/{1}/{2}/public/results/{3}'.format(
+                                config['taskcluster']['artifacts'],
+                                _job['task_id'],
+                                _job['retry_id'],
+                                config['artifacts']['report']
+                            )
+                        ) as resp:
+                            source = resp.read()
+                            data = xmltodict.parse(source)
+                            root = data['testsuites']
+                            for child in root['testsuite']:
+                                if child['@name'] != 'junit-ignored':
+                                    if child['@failures'] == '1':
+                                        _test_details.append(
+                                            {
+                                                'name': child['testcase']
+                                                ['@name'],
+                                                'result': 'failure'
+                                            }
+                                        )
+                                    elif child['@flakes'] == '1':
+                                        _test_details.append(
+                                            {
+                                                'name': child['testcase']
+                                                ['@name'],
+                                                'result': 'flaky'
+                                            }
+                                        )
+                                    else:
+                                        pass
+                    else:
+                        pass
+
+                    # TaskCluster: payload mobile revision
+                    with request.urlopen('{0}/api/queue/v1/task/{1}/'.format(
+                            config['taskcluster']['host'],
+                            _job['task_id']
                         )
                     ) as resp:
                         source = resp.read()
                         data = json.loads(source)
-                        for key, value in data.items():
-                            _matrix_general_details = {
-                                "webLink": value[
-                                    'webLinkWithoutExecutionDetails'
-                                ]
-                            }
-                            _matrix_outcome_details = value['testAxises'][0]
+                        _revSHA = data['payload']['env']['MOBILE_HEAD_REV']
+                except urllib.error.URLError as err:
+                    raise SystemExit(err)
 
-                    # JUnitReport
+                # Github
+                try:
                     with request.urlopen(
-                        '{0}/{1}/{2}/public/results/{3}'.format(
-                            config['taskcluster']['artifacts'],
-                            _job['task_id'],
-                            _job['retry_id'],
-                            config['artifacts']['report']
+                        request.Request(
+                            url='{0}{1}/commits/{2}/pulls'.format(
+                                config['project']['url'],
+                                args.project,
+                                _revSHA
+                            ),
+                            headers={
+                                'Accept':
+                                'application/vnd.github.groot-preview+json',
+                                'Authorization':
+                                'token %s' % os.environ['GITHUB_TOKEN']
+                            }
                         )
                     ) as resp:
                         source = resp.read()
-                        data = xmltodict.parse(source)
-                        root = data['testsuites']
-                        for child in root['testsuite']:
-                            if child['@name'] != 'junit-ignored':
-                                if child['@failures'] == '1':
-                                    _test_details.append(
-                                        {
-                                            'name': child['testcase']['@name'],
-                                            'result': 'failure'
-                                        }
-                                    )
-                                elif child['@flakes'] == '1':
-                                    _test_details.append(
-                                        {
-                                            'name': child['testcase']['@name'],
-                                            'result': 'flaky'
-                                        }
-                                    )
-                                else:
-                                    pass
-                else:
-                    pass
+                        _github_data = json.loads(source)
+                        for _data in _github_data:
+                            _github_data = _data
 
-                # TaskCluster: payload mobile revision
-                with request.urlopen('{0}/api/queue/v1/task/{1}/'.format(
+                except urllib.error.URLError as err:
+                    raise SystemExit(err)
+
+                dt_obj_start = datetime.fromtimestamp(_job['start_timestamp'])
+                dt_obj_end = datetime.fromtimestamp(_job['end_timestamp'])
+
+                durations.append(
+                    (dt_obj_end - dt_obj_start).total_seconds() / 60)
+                outcomes.append(_job)
+                dataset.append({
+                    'push_id': _push['id'],
+                    'task_id': _job['task_id'],
+                    'duration': '{0:.0f}'.format(
+                        (dt_obj_end - dt_obj_start).total_seconds() / 60
+                    ),
+                    'author': _job['who'],
+                    'result': _job['result'],
+                    'task_html_url': '{0}'.format(
+                        config['taskcluster']['host']
+                        + '/tasks/' + _job['task_id']
+                    ),
+                    'last_modified': _job['last_modified'],
+                    'task_log': _log,
+                    'matrix_general_details': _matrix_general_details,
+                    'matrix_outcome_details': _matrix_outcome_details,
+                    'revision': _revSHA,
+                    'pullreq_html_url': _github_data['html_url']
+                    if _github_data else None,
+                    'pullreq_html_title': _github_data['title']
+                    if _github_data else None,
+                    'problem_test_details': _test_details
+                })
+
+                logger.info(
+                    'Duration: {0:.0f} min {1} - {2} - '
+                    '{3}/tasks/{4} - {5} - {6} - {7} - '
+                    '{8} - {9} - {10} - {11} - {12} - {13}\n'.format(
+                        (dt_obj_end - dt_obj_start).total_seconds() / 60,
+                        _job['who'],
+                        _job['result'],
                         config['taskcluster']['host'],
-                        _job['task_id']
+                        _job['task_id'],
+                        _job['last_modified'],
+                        _log,
+                        _matrix_outcome_details['details']
+                        if _matrix_outcome_details else None,
+                        _matrix_outcome_details['outcome']
+                        if _matrix_outcome_details else None,
+                        _matrix_general_details['webLink'],
+                        _revSHA,
+                        _test_details,
+                        _github_data['html_url'] if _github_data else None,
+                        _github_data['title'] if _github_data else None
                     )
-                ) as resp:
-                    source = resp.read()
-                    data = json.loads(source)
-                    _revSHA = data['payload']['env']['MOBILE_HEAD_REV']
-            except urllib.error.URLError as err:
-                raise SystemExit(err)
+                )
 
-            # Github
-            try:
-                with request.urlopen(
-                    request.Request(
-                        url='{0}{1}/commits/{2}/pulls'.format(
-                            config['project']['url'],
-                            args.project,
-                            _revSHA
-                        ),
-                        headers={
-                            'Accept':
-                                'application/vnd.github.groot-preview+json',
-                            'Authorization':
-                                'token %s' % os.environ['GITHUB_TOKEN']
-                        }
-                    )
-                ) as resp:
-                    source = resp.read()
-                    _github_data = json.loads(source)
-                    for _data in _github_data:
-                        _github_data = _data
+        if durations and outcomes and dataset:
 
-            except urllib.error.URLError as err:
-                raise SystemExit(err)
-
-            dt_obj_start = datetime.fromtimestamp(_job['start_timestamp'])
-            dt_obj_end = datetime.fromtimestamp(_job['end_timestamp'])
-
-            durations.append((dt_obj_end - dt_obj_start).total_seconds() / 60)
-            outcomes.append(_job)
-            dataset.append({
-                'push_id': _push['id'],
-                'task_id': _job['task_id'],
-                'duration': '{0:.0f}'.format(
-                    (dt_obj_end - dt_obj_start).total_seconds() / 60
-                ),
-                'author': _job['who'],
-                'result': _job['result'],
-                'task_html_url': '{0}'.format(
-                    config['taskcluster']['host'] + '/tasks/' + _job['task_id']
-                ),
-                'last_modified': _job['last_modified'],
-                'task_log': _log,
-                'matrix_general_details': _matrix_general_details,
-                'matrix_outcome_details': _matrix_outcome_details,
-                'revision': _revSHA,
-                'pullreq_html_url': _github_data['html_url'],
-                'pullreq_html_title': _github_data['title'],
-                'problem_test_details': _test_details
-            })
-
-            logger.info(
-                'Duration: {0:.0f} min {1} - {2} - '
-                '{3}/tasks/{4} - {5} - {6} - {7} - '
-                '{8} - {9} - {10} - {11} - {12} - {13}\n'.format(
-                    (dt_obj_end - dt_obj_start).total_seconds() / 60,
-                    _job['who'],
-                    _job['result'],
-                    config['taskcluster']['host'],
-                    _job['task_id'],
-                    _job['last_modified'],
-                    _log,
-                    _matrix_outcome_details['details']
-                    if _matrix_outcome_details else None,
-                    _matrix_outcome_details['outcome']
-                    if _matrix_outcome_details else None,
-                    _matrix_general_details['webLink'],
-                    _revSHA,
-                    _test_details,
-                    _github_data['html_url'],
-                    _github_data['title']
+            summary_set = {
+                'dataset_results': dataset,
+                'repo': args.project,
+                'job_symbol': project_config[job]['symbol'],
+                'job_result': project_config[job]['result'],
+                'job_duration_avg': round(mean(durations), 2),
+                'outcome_count': len(outcomes)
+            }
+            logger.info('Summary')
+            logger.info('Duration average: {0:.0f} minutes'.format(
+                    summary_set['job_duration_avg']
                 )
             )
+            logger.info('Results: {0} '.format(summary_set['outcome_count']))
+            print('Output written to LOG file', end='\n')
 
-    if durations and outcomes and dataset:
-
-        summary_set = {
-            'dataset_results': dataset,
-            'repo': args.project,
-            'job_symbol': project_config['job']['symbol'],
-            'job_result': project_config['job']['result'],
-            'job_duration_avg': round(mean(durations), 2),
-            'outcome_count': len(outcomes)
-        }
-        logger.info('Summary')
-        logger.info('Duration average: {0:.0f} minutes'.format(
-                summary_set['job_duration_avg']
-            )
-        )
-        logger.info('Results: {0} '.format(summary_set['outcome_count']))
-        print('Output written to LOG file', end='\n')
-
-        try:
-            with open('output.json', 'w') as outfile:
-                json.dump(summary_set, outfile, indent=4)
-                print('Output written to JSON file\n', end='\n')
-        except OSError as err:
-            raise SystemExit(err)
-    else:
-        print('No results found with provided config.')
+            try:
+                with open('output.json', 'w') as outfile:
+                    json.dump(summary_set, outfile, indent=4)
+                    print('Output written to JSON file\n', end='\n')
+            except OSError as err:
+                raise SystemExit(err)
+        else:
+            print('No results found with provided config.')
 
 
 if __name__ == '__main__':
